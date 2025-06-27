2ba900a80536440957231dff0741ed1d0daca054
devansh sachdeva <devansh.sachdeva09@gmail.com>
2025-06-27 16:38:27 +0530

    this is my 3rd python project word counter

--- 03_word counter_basic.py
+++ 03_word counter_basic.py
@@ -1,9 +1,9 @@
-
+#function for count word
 def count_words(text):
     words = text.split()
     return len(words)  # ✅ Return the word count
 
-
+#text
 text = input("Enter your text: ")
 count = count_words(text)
 print("Word Count:", count)
