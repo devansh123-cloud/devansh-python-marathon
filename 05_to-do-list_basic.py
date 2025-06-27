67e5230ced9b6ab521a0f50cd424dba0bc69fc2c
devansh sachdeva <devansh.sachdeva09@gmail.com>
2025-06-27 16:39:44 +0530

    this is my 5th project to-do list/task manager basic level

--- 05_to-do-list_basic.py
+++ 05_to-do-list_basic.py
@@ -1,11 +1,14 @@
 #task store
 tasks = []
+
 #count
 count = int(input("How many task? "))
+
 #range
 for i in range(1, count+1):
     task = input(f"Enter a task {i} :")
     tasks.append(task)
+    
 #print
 print("\nYour To-Do List:")
 for i, task in enumerate(tasks, start=1):
