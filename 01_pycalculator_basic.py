f3b2eeb6c0b7a744aff0c337b0c1da1618653c0c
devansh sachdeva <devansh.sachdeva09@gmail.com>
2025-06-27 16:33:49 +0530

    this is my 1st project python calculator basic level

--- 01_pycalculator_basic.py
+++ 01_pycalculator_basic.py
@@ -1,9 +1,10 @@
-
+#print heading
 print("Welcome to PythonCalc 🧮")
+#sum of numbers and operators
 n1 = int(input("Enter first number:"))
 operator = input("Enter operator(+, -, *, /):")
 n2 = int(input("Enter second number:"))
-
+#if/else function
 if operator == "+":
     print(f"Result:", n1+n2)
 
