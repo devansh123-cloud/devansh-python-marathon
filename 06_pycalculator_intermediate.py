542818a509d2b25942057d0f00f5df5364c85556
devansh sachdeva <devansh.sachdeva09@gmail.com>
2025-06-28 10:19:02 +0530

    this is my pycalculator project intermediate level

---
+++ 06_pycalculator_intermediate.py
@@ -0,0 +1,54 @@
+# --- PyCalculator ---
+# Choose operation (+, -, *, /) or type 'exit' to quit
+# Operation: +
+# Enter first number: 10
+# Enter second number: 5
+# Result: 15.0 ✅
+
+# Operation: *
+# Enter first number: 4
+# Enter second number: 0
+# Result: 0.0 ✅
+
+# Operation: hello
+# ❌ Invalid operation. Try again.
+
+# Operation: exit
+# Thanks for using PyCalculator!
+
+print("--- PyCalculator Intermediate ---")
+while True:
+    operation = input("Choose operation (+, -, *, /) or type 'exit' to quit : ").strip()
+    
+    if operation == "exit":
+        print("Thanks for using PyCalculator")
+        break
+        
+    
+    if operation not in ["+", "-", "/", "*"]:
+        print("Invalid operation. Try again.")
+        
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Please enter valid number \n")
+        continue
+    
+    if operation == "+":
+        result = num1 + num2
+    
+    elif operation == "-":
+        result = num1 - num2
+    
+    elif operation == "/":
+        result = num1 / num2
+        
+        if num2 == 0:
+            print("❌ Cannot divide by zero.\n")
+    
+    elif operation == "*":
+        result = num1 * num2
+    
+    print(f"Result {result} ✅\n")
+        
